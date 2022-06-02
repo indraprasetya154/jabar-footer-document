7df88efe4e2aec4d8f57ec2fd2a5cb2b41c68e31
Indra Prasetya <indraprasetya154@gmail.com>
2022-06-02 20:04:28 +0700

    Add footer document text and hyperlink base on qrcode value

--- src/controllers/pdf.js
+++ src/controllers/pdf.js
@@ -22,8 +22,8 @@ export const addFooterPdf = async (req, res) => {
                 errorCorrectionLevel: 'H',
             }).then((url) => {
                 return url
-            });  
-            
+            });
+
         const QRCodeImagePng = await pdfDoc.embedPng(QRCodeImage)
         const QRCodeImageLogo = await pdfDoc.embedPng(fs.readFileSync('./images/specimen-with-bg.png'))
         // Get the each page of the document
@@ -41,47 +41,62 @@ export const addFooterPdf = async (req, res) => {
                     // Draw a string of text diagonally across the each page
                     page.drawText('Dokumen ini telah ditandatangani secara elektronik menggunakan sertifikat elektronik yang diterbitkan oleh Balai Sertifikasi', {
                         x: 111,
-                        y: 70,
+                        y: 53,
                         size: 8,
                         font: arialRegularFont,
                         color: rgb(0, 0, 0),
                     })
                     page.drawText('Elektronik (BSrE) Badan Siber dan Sandi Negara. Dokumen digital yang asli dapat diperoleh dengan memindai QR Code atau', {
                         x: 105,
-                        y: 60,
+                        y: 43,
                         size: 8,
                         font: arialRegularFont,
                         color: rgb(0, 0, 0),
                     })
                     page.drawText('memasukkan kode pada Aplikasi TNDE Pemerintah Daerah Provinsi Jawa Barat.', {
                         x: 185,
-                        y: 50,
+                        y: 33,
                         size: 8,
                         font: arialRegularFont,
                         color: rgb(0, 0, 0),
                     })
-                    // Draw the QRCode
-                    page.drawImage(QRCodeImagePng, {
-                        x: 40,
-                        y: 40,
-                        width: 45,
-                        height: 45,
-                    }) 
-                    // Draw the Logo QRCode Manually
-                    page.drawImage(QRCodeImageLogo, {
-                        x: 53,
-                        y: 53,
-                        width: 20,
-                        height: 20,
-                    }) 
-                    // Add title on bottom QRCode
-                    page.drawText(req.body.code, {
-                        x: 37,
-                        y: 30,
+                    // Draw text link footer
+                    page.drawText('Untuk mengecek keaslian surat silahkan klik link ', {
+                        x: 157,
+                        y: 23,
                         size: 8,
                         font: arialRegularFont,
                         color: rgb(0, 0, 0),
-                    })   
+                    })
+                    page.drawText(req.body.qrcode, {
+                        x: 330,
+                        y: 23,
+                        size: 8,
+                        font: arialRegularFont,
+                        color: rgb(30/255, 136/255, 229/255),
+                    })
+                    // Draw the QRCode
+                    page.drawImage(QRCodeImagePng, {
+                        x: 40,
+                        y: 20,
+                        width: 45,
+                        height: 45,
+                    })
+                    // Draw the Logo QRCode Manually
+                    page.drawImage(QRCodeImageLogo, {
+                        x: 53,
+                        y: 33,
+                        width: 20,
+                        height: 20,
+                    })
+                    // Add title on bottom QRCode
+                    page.drawText(req.body.code, {
+                        x: 37,
+                        y: 10,
+                        size: 8,
+                        font: arialRegularFont,
+                        color: rgb(0, 0, 0),
+                    })
                 }
 
                 break;
@@ -91,40 +106,55 @@ export const addFooterPdf = async (req, res) => {
                     // Draw a string of text diagonally across the each page
                     page.drawText('Dokumen ini telah ditandatangani secara elektronik menggunakan sertifikat elektronik yang diterbitkan oleh Balai Sertifikasi Elektronik (BSrE) Badan Siber dan Sandi Negara.', {
                         x: 115,
-                        y: 25,
+                        y: 28,
                         size: 8,
                         font: arialRegularFont,
                         color: rgb(0, 0, 0),
                     })
                     page.drawText('Dokumen digital yang asli dapat diperoleh dengan memindai QR Code atau memasukkan kode pada Aplikasi TNDE Pemerintah Daerah Provinsi Jawa Barat.', {
                         x: 135,
-                        y: 15,
+                        y: 18,
                         size: 8,
                         font: arialRegularFont,
                         color: rgb(0, 0, 0),
                     })
+                    // Draw text link footer
+                    page.drawText('Untuk mengecek keaslian surat silahkan klik link ', {
+                        x: 228,
+                        y: 8,
+                        size: 8,
+                        font: arialRegularFont,
+                        color: rgb(0, 0, 0),
+                    })
+                    page.drawText(req.body.qrcode, {
+                        x: 402,
+                        y: 8,
+                        size: 8,
+                        font: arialRegularFont,
+                        color: rgb(30/255, 136/255, 229/255),
+                    })
                     // Draw the QRCode
                     page.drawImage(QRCodeImagePng, {
                         x: 117,
-                        y: 57,
+                        y: 52,
                         width: 40,
                         height: 40,
-                    }) 
+                    })
                     // Draw the Logo QRCode Manually
                     page.drawImage(QRCodeImageLogo, {
                         x: 130,
-                        y: 70,
+                        y: 65,
                         width: 15,
                         height: 15,
-                    }) 
+                    })
                     // Add title on bottom QRCode
                     page.drawText(req.body.code, {
-                        x: 114,
-                        y: 47,
+                        x: 115,
+                        y: 42,
                         size: 7,
                         font: arialRegularFont,
                         color: rgb(0, 0, 0),
-                    })   
+                    })
                 }
 
                 break;
@@ -146,20 +176,35 @@ export const addFooterPdf = async (req, res) => {
                         font: arialRegularFont,
                         color: rgb(0, 0, 0),
                     })
+                    // Draw text link footer
+                    page.drawText('Untuk mengecek keaslian surat silahkan klik link ', {
+                        x: 328,
+                        y: 20,
+                        size: 8,
+                        font: arialRegularFont,
+                        color: rgb(0, 0, 0),
+                    })
+                    page.drawText(req.body.qrcode, {
+                        x: 502,
+                        y: 20,
+                        size: 8,
+                        font: arialRegularFont,
+                        color: rgb(30/255, 136/255, 229/255),
+                    })
                     // Draw the QRCode
                     page.drawImage(QRCodeImagePng, {
                         x: 50,
                         y: 17,
                         width: 40,
                         height: 40,
-                    }) 
+                    })
                     // Draw the Logo QRCode Manually
                     page.drawImage(QRCodeImageLogo, {
                         x: 63,
                         y: 30,
                         width: 15,
                         height: 15,
-                    }) 
+                    })
                     // Add title on bottom QRCode
                     page.drawText(req.body.code, {
                         x: 47,
@@ -167,7 +212,7 @@ export const addFooterPdf = async (req, res) => {
                         size: 7,
                         font: arialRegularFont,
                         color: rgb(0, 0, 0),
-                    })   
+                    })
                 }
 
                 break;
@@ -177,47 +222,62 @@ export const addFooterPdf = async (req, res) => {
                     // Draw a string of text diagonally across the each page
                     page.drawText('Dokumen ini telah ditandatangani secara elektronik menggunakan sertifikat elektronik yang diterbitkan oleh Balai Sertifikasi Elektronik (BSrE) Badan', {
                         x: 90,
-                        y: 34,
+                        y: 50,
                         size: 7,
                         font: arialRegularFont,
                         color: rgb(0, 0, 0),
                     })
                     page.drawText('Siber dan Sandi Negara. Dokumen digital yang asli dapat diperoleh dengan memindai QR Code atau memasukkan kode pada Aplikasi TNDE', {
                         x: 100,
-                        y: 24,
+                        y: 40,
                         size: 7,
                         font: arialRegularFont,
                         color: rgb(0, 0, 0),
                     })
                     page.drawText('Pemerintah Daerah Provinsi Jawa Barat.', {
                         x: 255,
-                        y: 14,
+                        y: 30,
                         size: 7,
                         font: arialRegularFont,
                         color: rgb(0, 0, 0),
                     })
+                    // Draw text link footer
+                    page.drawText('Untuk mengecek keaslian surat silahkan klik link ', {
+                        x: 178,
+                        y: 20,
+                        size: 7,
+                        font: arialRegularFont,
+                        color: rgb(0, 0, 0),
+                    })
+                    page.drawText(req.body.qrcode, {
+                        x: 330,
+                        y: 20,
+                        size: 7,
+                        font: arialRegularFont,
+                        color: rgb(30/255, 136/255, 229/255),
+                    })
                     // Draw the QRCode
                     page.drawImage(QRCodeImagePng, {
                         x: 35,
-                        y: 15,
-                        width: 30,
-                        height: 30,
-                    }) 
+                        y: 20,
+                        width: 40,
+                        height: 40,
+                    })
                     // Draw the Logo QRCode Manually
                     page.drawImage(QRCodeImageLogo, {
-                        x: 44,
-                        y: 24,
+                        x: 50,
+                        y: 35,
                         width: 12,
                         height: 12,
-                    }) 
+                    })
                     // Add title on bottom QRCode
                     page.drawText(req.body.code, {
-                        x: 31,
-                        y: 8,
-                        size: 6,
+                        x: 33,
+                        y: 13,
+                        size: 7,
                         font: arialRegularFont,
                         color: rgb(0, 0, 0),
-                    })   
+                    })
                 }
 
                 break;
@@ -239,20 +299,35 @@ export const addFooterPdf = async (req, res) => {
                             font: arialRegularFont,
                             color: rgb(0, 0, 0),
                         })
+                        // Draw text link footer
+                        page.drawText('Untuk mengecek keaslian surat silahkan klik link ', {
+                            x: 288,
+                            y: 20,
+                            size: 8,
+                            font: arialRegularFont,
+                            color: rgb(0, 0, 0),
+                        })
+                        page.drawText(req.body.qrcode, {
+                            x: 462,
+                            y: 20,
+                            size: 8,
+                            font: arialRegularFont,
+                            color: rgb(30/255, 136/255, 229/255),
+                        })
                         // Draw the QRCode
                         page.drawImage(QRCodeImagePng, {
                             x: 70,
                             y: 17,
                             width: 40,
                             height: 40,
-                        }) 
+                        })
                         // Draw the Logo QRCode Manually
                         page.drawImage(QRCodeImageLogo, {
                             x: 83,
                             y: 30,
                             width: 15,
                             height: 15,
-                        }) 
+                        })
                         // Add title on bottom QRCode
                         page.drawText(req.body.code, {
                             x: 67,
@@ -260,18 +335,17 @@ export const addFooterPdf = async (req, res) => {
                             size: 7,
                             font: arialRegularFont,
                             color: rgb(0, 0, 0),
-                        })   
+                        })
                     }
-        
+
                     break;
-        
+
             default:
                 res.status(400).json({
                     message: 'Template type not yet supported'
                 });
                 break;
         }
-            
         // Serialize the PDFDocument to bytes (a Uint8Array)
         const pdfBytes = await pdfDoc.save()
         let pdfBuffer = Buffer.from(pdfBytes.buffer, 'binary');
@@ -282,7 +356,7 @@ export const addFooterPdf = async (req, res) => {
         res.send(pdfBuffer);
     } catch (error) {
         res.status(400).json({
-            message: 'Error while generating PDF',
+            message: 'Error while generating PDF. Message: ' + error.message + '. Detail: ' + error.stack,
         });
     }
 }
