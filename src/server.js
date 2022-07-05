aff4e4418092f15d1ebf43005fbc3451bfc1cf5f
Indra Prasetya <indraprasetya154@gmail.com>
2022-07-05 08:42:08 +0700

    remove unused code

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import routes from './routes/index.js';
-import path from 'path';
 
 const app = express();
 
@@ -15,6 +14,5 @@ app.use(cors());
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
-app.use(express.static(path.resolve('public')));
 
 app.listen(PORT, console.log(`Server running in ${ENV} mode on port ${PORT}`));
