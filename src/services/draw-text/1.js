ccad49e019f5b638db205621e8c9ef12fd866eed
Fajar Hikmal <fajarhikmal214@gmail.com>
2022-07-08 14:12:10 +0700

    refactor(draw-text): Move draw text function to services

---
+++ src/services/draw-text/1.js
@@ -0,0 +1,63 @@
+import { rgb } from 'pdf-lib';
+
+export function drawText1(req, page, arialRegularFont, QRCodeImagePng, QRCodeImageLogo, footerUrl) {
+    // Draw a string of text diagonally across the each page
+    page.drawText('Dokumen ini telah ditandatangani secara elektronik menggunakan sertifikat elektronik yang diterbitkan oleh Balai Sertifikasi', {
+        x: 111,
+        y: 53-10,
+        size: 8,
+        font: arialRegularFont,
+        color: rgb(0, 0, 0),
+    })
+    page.drawText('Elektronik (BSrE) Badan Siber dan Sandi Negara. Dokumen digital yang asli dapat diperoleh dengan memindai QR Code atau', {
+        x: 105,
+        y: 43-10,
+        size: 8,
+        font: arialRegularFont,
+        color: rgb(0, 0, 0),
+    })
+    page.drawText('memasukkan kode pada Aplikasi NDE Pemerintah Daerah Provinsi Jawa Barat.', {
+        x: 185,
+        y: 33-10,
+        size: 8,
+        font: arialRegularFont,
+        color: rgb(0, 0, 0),
+    })
+    // Draw text link footer
+    // page.drawText('Untuk mengecek keaslian surat silahkan klik link ', {
+    //     x: 157,
+    //     y: 23,
+    //     size: 8,
+    //     font: arialRegularFont,
+    //     color: rgb(0, 0, 0),
+    // })
+    // page.drawText(footerUrl, {
+    //     x: 330,
+    //     y: 23,
+    //     size: 8,
+    //     font: arialRegularFont,
+    //     color: rgb(30/255, 136/255, 229/255),
+    // })
+    // Draw the QRCode
+    page.drawImage(QRCodeImagePng, {
+        x: 40,
+        y: 20,
+        width: 45,
+        height: 45,
+    })
+    // Draw the Logo QRCode Manually
+    page.drawImage(QRCodeImageLogo, {
+        x: 53,
+        y: 33,
+        width: 20,
+        height: 20,
+    })
+    // Add title on bottom QRCode
+    page.drawText(req.body.code, {
+        x: 37,
+        y: 10,
+        size: 8,
+        font: arialRegularFont,
+        color: rgb(0, 0, 0),
+    })
+}
---
+++ src/services/draw-text/3.js
@@ -0,0 +1,56 @@
+import { rgb } from 'pdf-lib';
+
+export function drawText3(req, page, arialRegularFont, QRCodeImagePng, QRCodeImageLogo, footerUrl) {
+    // Draw a string of text diagonally across the each page
+    page.drawText('Dokumen ini telah ditandatangani secara elektronik menggunakan sertifikat elektronik yang diterbitkan oleh Balai Sertifikasi Elektronik (BSrE) Badan Siber dan Sandi Negara. Dokumen digital', {
+        x: 180,
+        y: 40-10,
+        size: 8,
+        font: arialRegularFont,
+        color: rgb(0, 0, 0),
+    })
+    page.drawText('yang asli dapat diperoleh dengan memindai QR Code atau memasukkan kode pada Aplikasi NDE Pemerintah Daerah Provinsi Jawa Barat.', {
+        x: 265,
+        y: 30-10,
+        size: 8,
+        font: arialRegularFont,
+        color: rgb(0, 0, 0),
+    })
+    // Draw text link footer
+    // page.drawText('Untuk mengecek keaslian surat silahkan klik link ', {
+    //     x: 328,
+    //     y: 20,
+    //     size: 8,
+    //     font: arialRegularFont,
+    //     color: rgb(0, 0, 0),
+    // })
+    // page.drawText(footerUrl, {
+    //     x: 502,
+    //     y: 20,
+    //     size: 8,
+    //     font: arialRegularFont,
+    //     color: rgb(30/255, 136/255, 229/255),
+    // })
+    // Draw the QRCode
+    page.drawImage(QRCodeImagePng, {
+        x: 50,
+        y: 17,
+        width: 40,
+        height: 40,
+    })
+    // Draw the Logo QRCode Manually
+    page.drawImage(QRCodeImageLogo, {
+        x: 63,
+        y: 30,
+        width: 15,
+        height: 15,
+    })
+    // Add title on bottom QRCode
+    page.drawText(req.body.code, {
+        x: 47,
+        y: 7,
+        size: 7,
+        font: arialRegularFont,
+        color: rgb(0, 0, 0),
+    })
+}
